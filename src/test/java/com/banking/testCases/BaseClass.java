0269fb7461d6de66c7e8fdc03bb9d861854ec221
anil2105 <anil.krtg@gmail.com>
2022-11-05 16:30:51 +0530

    This is my first commit

---
+++ src/test/java/com/banking/testCases/BaseClass.java
@@ -0,0 +1,56 @@
+package com.banking.testCases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import com.banking.utilities.ReadConfig;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+
+	ReadConfig readconfig = new ReadConfig();
+
+	public String baseURL = readconfig.getApplicationURL();
+	public String username = readconfig.getUsername();
+	public String password = readconfig.getPassword();
+	public static WebDriver driver;
+
+	public static Logger logger;
+
+	@Parameters("browser")
+	@BeforeClass
+	public void setup(String br) {
+		logger = Logger.getLogger("ebanking");
+		PropertyConfigurator.configure("Log4j.properties");
+
+		if (br.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (br.equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else if (br.equals("ie")) {
+			WebDriverManager.iedriver().setup();
+			driver = new InternetExplorerDriver();
+		}
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get(baseURL);
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
